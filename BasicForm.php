ef4ef0069a1b090697f86b5f39ff2486390a6ed8
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-11-21 18:09:17 +0530

    Added active check.

--- BasicForm.php
+++ BasicForm.php
@@ -26,62 +26,69 @@ class BasicForm extends BaseForm {
 	// BasicForm
 
     public function renderForm() {
+		
+		if( isset( $this->form ) && $this->form->active ) {
 
-		$model		= $this->model;
-		$fields 	= $this->model->fields;
-		$activeForm	= $this->activeForm;
-
-		foreach ( $fields as $key => $field ) {
-
-			$fieldHtml	= null;
-
-			if( isset( $field->options ) ) {
-
-				$field->options	= json_decode( $field->options, true );
+			$model		= $this->model;
+			$fields 	= $this->model->fields;
+			$activeForm	= $this->activeForm;
+	
+			foreach ( $fields as $key => $field ) {
+	
+				$fieldHtml	= null;
+	
+				if( isset( $field->options ) ) {
+	
+					$field->options	= json_decode( $field->options, true );
+				}
+				else {
+	
+					$field->options	= [];
+				}
+	
+				switch( $field->type ) {
+	
+					case FormField::TYPE_TEXT: {
+	
+						$fieldHtml = $activeForm->field( $model, $key )->textInput( $field->options );
+	
+						break;
+					}
+					case FormField::TYPE_TEXTAREA: {
+	
+						$fieldHtml = $activeForm->field( $model, $key )->textArea( $field->options );
+	
+						break;
+					}
+				}
+	
+				if( !isset( $field->label ) ) {
+	
+					$fieldHtml->label( false );
+				}
+	
+				echo $fieldHtml;
+			}
+	
+			if( $this->form->captcha ) {
+	
+				$captchaHtml = $activeForm->field( $model, 'captcha' )->label( false )->widget( Captcha::classname(), [ 'options' => [ 'placeholder' => 'Captcha*' ] ] );
+	
+				echo $captchaHtml;
+			}
+			
+			if( !isset( $this->formActions ) ) {
+	
+				echo "<div class='frm-actions'><input type='submit' value='Submit' /></div>";
 			}
 			else {
-
-				$field->options	= [];
+				
+				echo $this->formActions;
 			}
-
-			switch( $field->type ) {
-
-				case FormField::TYPE_TEXT: {
-
-					$fieldHtml = $activeForm->field( $model, $key )->textInput( $field->options );
-
-					break;
-				}
-				case FormField::TYPE_TEXTAREA: {
-
-					$fieldHtml = $activeForm->field( $model, $key )->textArea( $field->options );
-
-					break;
-				}
-			}
-
-			if( !isset( $field->label ) ) {
-
-				$fieldHtml->label( false );
-			}
-
-			echo $fieldHtml;
-		}
-
-		if( $this->form->captcha ) {
-
-			$captchaHtml = $activeForm->field( $model, 'captcha' )->label( false )->widget( Captcha::classname(), [ 'options' => [ 'placeholder' => 'Captcha*' ] ] );
-
-			echo $captchaHtml;
-		}
-		
-		if( !isset( $this->formActions ) ) {
-
-			echo "<div class='frm-actions'><input type='submit' value='Submit' /></div>";
 		}
 		else {
 			
-			echo $this->formActions;
+			echo "<div class='warning'>Form submission is disabled by site admin.</div>";	
 		}
     }
 }
